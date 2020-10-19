6f0fe4027c3567ab027fc7fe84f475e687bfc0fc
Yusufbeskan <yusufemre140@gmail.com>
2020-10-19 09:54:24 +0000

    last ex

---
+++ lab1/ex4.py
@@ -0,0 +1,4 @@
+slide = int(input("Enter the value of slide:" ))
+side = int(input("Enter the value of side:" ))
+hypotenus = (slide**2 + side**2)**0.5
+print(hypotenus)
---
+++ lab1/ex5.py
@@ -0,0 +1,8 @@
+velocity1 = 80
+velocity2 = 70
+distance1 = 490
+distance2 = 150
+hour = (distance1 - distance2) / (velocity1 + velocity2 )
+minutes = hour * 60
+print(minutes)
+
