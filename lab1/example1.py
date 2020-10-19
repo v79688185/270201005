2ad0a231edc0cf60fe83eb6ec31df703621d4cb3
Yusufbeskan <yusufemre140@gmail.com>
2020-10-19 09:25:41 +0000

    ex 4

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,7 +1,3 @@
-a=2
-b=6
-c=-20
-d = b**2-4*a*c
-sol1 = (-b + d**0.5)/(2*a)
-sol2 = (-b- d**0.5)/(2*a)
-print(sol1,sol2)
+temperatureC=int(input("Enter the temperature:"))
+temperatureF = temperatureC * 1.8 + 32
+print("Fahrenite","is", temperatureF,)
